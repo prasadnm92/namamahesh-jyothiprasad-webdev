508b21b19a53287b3f9ac47ea86be35be1ad8cb2
Jyothi Prasad Nama Mahesh <prasadnm@ccs.neu.edu>
2016-10-14 15:32:13 -0400

    assignment3 setup

---
+++ public/assignment/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by prasadnm on 10/13/16.
+ */
+(function() {
+    angular
+        .module("WebAppMaker", ['ngRoute']);
+})();
---
+++ public/assignment/config.js
@@ -0,0 +1,20 @@
+/**
+ * Created by prasadnm on 10/13/16.
+ */
+(function() {
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            })
+            .otherwise({
+                redirectTo: "/login"
+            });
+    }
+})();
