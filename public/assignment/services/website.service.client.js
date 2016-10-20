3a563d21213536ada6e750dd33990c00758e22be
Jyothi Prasad Nama Mahesh <prasadnm@ccs.neu.edu>
2016-10-19 23:32:10 -0400

    finished website model

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -3,7 +3,7 @@
  */
 (function() {
     angular
-        .module('WebAppMaker')
+        .module("WebAppMaker")
         .factory("WebsiteService", WebsiteService);
 
     function WebsiteService() {
@@ -22,25 +22,56 @@
             updateWebsite       : updateWebsite,
             deleteWebsite       : deleteWebsite
         };
+        return api;
 
         function createWebsite(userId, website) {
-
+            for(var w in websites) {
+                if(websites[w].developerId===userId && websites[w].name===website.name) {
+                    return false;
+                }
+            }
+            website._id = parseInt(new Date().getTime());
+            website.developerId = userId;
+            websites.push(website);
+            return true;
         }
 
         function findWebsitesByUser(userId) {
-
+            var currentUserWebsites = [];
+            for(var w in websites) {
+                if(websites[w].developerId === userId) currentUserWebsites.push(websites[w]);
+            }
+            return currentUserWebsites;
         }
 
         function findWebsiteById(websiteId) {
-
+            for(var w in websites) {
+                if(websites[w]._id===websiteId) {
+                    return websites[w];
+                }
+            }
+            return undefined;
         }
 
         function updateWebsite(websiteId, website) {
-
+            for(var w in websites) {
+                if(websites[w]._id===websiteId) {
+                    website._id = websites[w]._id;
+                    websites[w] = website;
+                    return true;
+                }
+            }
+            return false;
         }
 
         function deleteWebsite(websiteId) {
-
+            for(var w in websites) {
+                if(websites[w]._id===websiteId) {
+                    websites.splice(w,1);
+                    return true;
+                }
+            }
+            return false;
         }
     }
 })();
--- public/assignment/views/website/website.controller.client.js
+++ public/assignment/views/website/website.controller.client.js
@@ -8,14 +8,47 @@
         .controller("NewWebsiteController", NewWebsiteController)
         .controller("EditWebsiteController", EditWebsiteController);
 
-    function WebsiteListController() {
-
+    function WebsiteListController($routeParams, WebsiteService) {
+        var vm = this;
+        vm.userId = parseInt($routeParams.uid);
+        vm.websites = WebsiteService.findWebsitesByUser(vm.userId);
     }
 
-    function NewWebsiteController() {
+    function NewWebsiteController($location, $routeParams, WebsiteService) {
+        var vm = this;
+        vm.userId = parseInt($routeParams.uid);
+        vm.websites = WebsiteService.findWebsitesByUser(vm.userId);
+        vm.createWebsite = createWebsite;
 
+        function createWebsite() {
+            if(!WebsiteService.createWebsite(vm.userId, vm.website)) {
+                vm.error = "Website with this title already exists"
+            }
+            if(!vm.error) $location.url("/user/"+vm.userId+"/website")
+        }
     }
 
-    function EditWebsiteController() {
+    function EditWebsiteController($location, $routeParams, WebsiteService) {
+        var vm = this;
+        vm.userId = parseInt($routeParams.uid);
+        vm.websites = WebsiteService.findWebsitesByUser(vm.userId);
+        vm.websiteId = parseInt($routeParams.wid);
+        vm.website = WebsiteService.findWebsiteById(vm.websiteId);
+        vm.createWebsite = updateWebsite;
+        vm.deleteWebsite = deleteWebsite;
+
+        function updateWebsite() {
+            if(!WebsiteService.updateWebsite(vm.websiteId, vm.website)) {
+                vm.error = "Could not update website";
+            }
+            if(!vm.error) $location.url("/user/"+vm.userId+"/website");
+        }
+
+        function deleteWebsite() {
+            if(!WebsiteService.deleteWebsite(vm.websiteId)) {
+                vm.error = "Could not delete website";
+            }
+            if(!vm.error) $location.url("/user/"+vm.userId+"/website");
+        }
     }
 })();
