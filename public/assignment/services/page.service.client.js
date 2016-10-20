de3582dc453434051faf42c8c50789625d445053
Jyothi Prasad Nama Mahesh <prasadnm@ccs.neu.edu>
2016-10-20 01:05:52 -0400

    finished page model

--- public/assignment/services/page.service.client.js
+++ public/assignment/services/page.service.client.js
@@ -19,25 +19,53 @@
             updatePage          : updatePage,
             deletePage          : deletePage
         };
+        return api;
 
         function createPage(websiteId, page) {
-
+            for(var p in pages) {
+                if(pages[p].websiteId===websiteId && pages[p].name===page.name) return false;
+            }
+            page._id = parseInt(new Date().getTime());
+            page.websiteId = websiteId;
+            pages.push(page);
+            return true;
         }
 
         function findPageByWebsiteId(websiteId) {
-
+            var currentWebsitePages = [];
+            for(var p in pages) {
+                if(pages[p].websiteId === websiteId) currentWebsitePages.push(pages[p]);
+            }
+            return currentWebsitePages;
         }
 
         function findPageById(pageId) {
-
+            for(var p in pages) {
+                if(pages[p]._id===pageId) return pages[p];
+            }
+            return undefined;
         }
 
         function updatePage(pageId, page) {
-
+            for(var p in pages) {
+                if(pages[p]._id===pageId) {
+                    page._id = pageId;
+                    page.websiteId = pages[p].websiteId;
+                    pages[p] = page;
+                    return true;
+                }
+            }
+            return false;
         }
 
         function deletePage(pageId) {
-
+            for(var p in pages) {
+                if(pages[p]._id===pageId) {
+                    pages.splice(p,1);
+                    return true;
+                }
+            }
+            return false;
         }
     }
 })();
--- public/assignment/views/page/page.controller.client.js
+++ public/assignment/views/page/page.controller.client.js
@@ -8,14 +8,50 @@
         .controller("NewPageController", NewPageController)
         .controller("EditPageController", EditPageController);
 
-    function PageListController() {
-
+    function PageListController($routeParams, PageService) {
+        var vm = this;
+        vm.userId = parseInt($routeParams.uid);
+        vm.websiteId = parseInt($routeParams.wid);
+        vm.pages = PageService.findPageByWebsiteId(vm.websiteId);
     }
 
-    function NewPageController() {
+    function NewPageController($location, $routeParams, PageService) {
+        var vm = this;
+        vm.userId = parseInt($routeParams.uid);
+        vm.websiteId = parseInt($routeParams.wid);
+        vm.pages = PageService.findPageByWebsiteId(vm.websiteId);
+        vm.createPage = createPage;
 
+        function createPage() {
+            if(!PageService.createPage(vm.websiteId, vm.page)) {
+                vm.error = "Page with this title already exists"
+            }
+            if(!vm.error) $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page");
+        }
     }
 
-    function EditPageController() {
+    function EditPageController($location, $routeParams, PageService) {
+        var vm = this;
+        vm.userId = parseInt($routeParams.uid);
+        vm.websiteId = parseInt($routeParams.wid);
+        vm.pages = PageService.findPageByWebsiteId(vm.websiteId);
+        vm.pageId = parseInt($routeParams.pid);
+        vm.page = PageService.findPageById(vm.pageId);
+        vm.updatePage = updatePage;
+        vm.deletePage = deletePage;
+
+        function updatePage() {
+            if(!PageService.updatePage(vm.pageId, vm.page)) {
+                vm.error = "Could not update page";
+            }
+            if(!vm.error) $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page");
+        }
+
+        function deletePage() {
+            if(!PageService.deletePage(vm.pageId)) {
+                vm.error = "Could not delete page";
+            }
+            if(!vm.error) $location.url("/user/"+vm.userId+"/website/"+vm.websiteId+"/page");
+        }
     }
 })();
