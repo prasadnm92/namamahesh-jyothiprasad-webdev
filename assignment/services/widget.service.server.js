50cafa571fa808b29ec2199c14f2d141e81d7b80
Jyothi Prasad Nama Mahesh <prasadnm@ccs.neu.edu>
2016-11-03 00:08:47 -0400

    finished widget web services

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -1,3 +1,78 @@
 /**
  * Created by prasadnm on 11/2/16.
  */
+module.exports = function(app) {
+    var widgets = [
+        { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
+        { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+            "url": "http://lorempixel.com/400/200/"},
+        { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+        { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+        { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+            "url": "https://youtu.be/AM2Ivdi9c4E" },
+        { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+    ];
+
+    app.post("/api/page/:pid/widget", createWidget);
+    app.get("/api/page/:pid/widget", findAllWidgetsForPage);
+    app.get("/api/widget/:wgid", findWidgetById);
+    app.put("/api/widget/:wgid", updateWidget);
+    app.delete("/api/widget/:wgid", deleteWidget);
+
+    function createWidget(req, res) {
+        var pageId = req.params.pid;
+        var widget = req.body;
+        widget.pageId = pageId;
+        widget._id = new Date().getTime().toString();
+        widgets.push(widget);
+        res.send(widget);
+    }
+
+    function findAllWidgetsForPage(req, res) {
+        var pageId = req.params.pid;
+        var pageWidgets = [];
+        for(var w in widgets) {
+            if(widgets[w].pageId === pageId) pageWidgets.push(widgets[w]);
+        }
+        res.send(pageWidgets);
+    }
+
+    function findWidgetById(req, res) {
+        var widgetId = req.params.wgid;
+        for(var w in widgets) {
+            if(widgets[w]._id === widgetId) {
+                res.send(widgets[w]);
+                return;
+            }
+        }
+        res.send(undefined);
+    }
+
+    function updateWidget(req, res) {
+        var widgetId = req.params.wgid;
+        var widget = req.body;
+        for(var w in widgets) {
+            if(widgets[w]._id === widgetId) {
+                widget._id = widgets[w]._id;
+                widget.pageId = widgets[w].pageId;
+                widgets[w] = widget;
+                res.send(widget);
+                return;
+            }
+        }
+        res.send(null);
+    }
+
+    function deleteWidget(req, res) {
+        var widgetId = req.params.wgid;
+        for(var w in widgets) {
+            if(widgets[w]._id === widgetId) {
+                widgets.splice(w,1);
+                res.send(true);
+                return;
+            }
+        }
+        res.send(false);
+    }
+};
--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -6,18 +6,7 @@
         .module('WebAppMaker')
         .factory("WidgetService", WidgetService);
 
-    function WidgetService() {
-        var widgets = [
-            { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
-            { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-                "url": "http://lorempixel.com/400/200/"},
-            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
-            { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
-            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
-        ];
+    function WidgetService($http) {
         var api = {
             createWidget          : createWidget,
             findWidgetsByPageId   : findWidgetsByPageId,
@@ -28,51 +17,23 @@
         return api;
 
         function createWidget(pageId, widget) {
-            for(var w in widgets) {
-                if(widgets[w].pageId === pageId) {
-                    widget.pageId = pageId;
-                    widget._id = new Date().getTime().toString();
-                    widgets.push(widget);
-                    return widget._id;
-                }
-            }
-            return undefined;
+            return $http.post("/api/page/"+pageId+"/widget", widget);
         }
 
         function findWidgetsByPageId(pageId) {
-            var currentPageWidgets = [];
-            for(var w in widgets) {
-                if(widgets[w].pageId === pageId) currentPageWidgets.push(widgets[w]);
-            }
-            return currentPageWidgets;
+            return $http.get("/api/page/"+pageId+"/widget");
         }
 
         function findWidgetById(widgetId) {
-            for(var w in widgets) {
-                if(widgets[w]._id === widgetId) return widgets[w];
-            }
-            return undefined;
+            return $http.get("/api/widget/"+widgetId);
         }
 
         function updateWidget(widgetId, widget) {
-            for(var w in widgets) {
-                if(widgets[w]._id === widgetId) {
-                    widget._id = widgets[w]._id;
-                    widget.pageId = widgets[w].pageId;
-                    return true;
-                }
-            }
-            return false;
+            return $http.put("/api/widget/"+widgetId, widget);
         }
 
         function deleteWidget(widgetId) {
-            for(var w in widgets) {
-                if(widgets[w]._id === widgetId) {
-                    widgets.splice(w,1);
-                    return true;
-                }
-            }
-            return false;
+            return $http.delete("/api/widget/"+widgetId);
         }
     }
 })();
