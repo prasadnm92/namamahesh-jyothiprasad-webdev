33973431ba91071b8501e7abc75a5840b8181446
Jyothi Prasad Nama Mahesh <prasadnm@ccs.neu.edu>
2016-11-04 15:56:45 -0400

    finished widget image upload support

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -2,6 +2,9 @@
  * Created by prasadnm on 11/2/16.
  */
 module.exports = function(app) {
+    var multer = require('multer');
+    var upload = multer({dest: __dirname+'/../../public/assignment/uploads'});
+
     var widgets = [
         { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
         { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
@@ -19,6 +22,7 @@ module.exports = function(app) {
     app.get("/api/widget/:wgid", findWidgetById);
     app.put("/api/widget/:wgid", updateWidget);
     app.delete("/api/widget/:wgid", deleteWidget);
+    app.post ("/api/upload", upload.single('myFile'), uploadImage);
 
     function createWidget(req, res) {
         var pageId = req.params.pid;
@@ -40,28 +44,13 @@ module.exports = function(app) {
 
     function findWidgetById(req, res) {
         var widgetId = req.params.wgid;
-        for(var w in widgets) {
-            if(widgets[w]._id === widgetId) {
-                res.send(widgets[w]);
-                return;
-            }
-        }
-        res.send(undefined);
+        res.send(getWidgetById(widgetId));
     }
 
     function updateWidget(req, res) {
         var widgetId = req.params.wgid;
         var widget = req.body;
-        for(var w in widgets) {
-            if(widgets[w]._id === widgetId) {
-                widget._id = widgets[w]._id;
-                widget.pageId = widgets[w].pageId;
-                widgets[w] = widget;
-                res.send(widget);
-                return;
-            }
-        }
-        res.send(null);
+        res.send(updateWidgetById(widgetId, widget));
     }
 
     function deleteWidget(req, res) {
@@ -74,5 +63,62 @@ module.exports = function(app) {
             }
         }
         res.send(false);
+    }
+
+    function uploadImage(req, res) {
+        var widgetId      = req.body.widgetId;
+        var userId        = req.body.userId;
+        var websiteId     = req.body.websiteId;
+        var pageId        = req.body.pageId;
+        var widget        = getWidgetById(widgetId);
+
+        //get the widget's attributes
+        var width         = req.body.width;
+        var name          = req.body.name;
+        var text          = req.body.text;
+
+        var metadata = {
+            originalname    : req.file.originalname, // file name on user's computer
+            filename        : req.file.filename,     // new file name in upload folder
+            fullPath        : req.file.path,         // full path of uploaded file
+            size            : req.file.size,
+            mimeType        : req.file.mimetype
+        };
+
+        var url = "uploads/"+metadata.filename;
+        widget.url = url;
+
+        widget.width = width;
+        widget.name = name;
+        widget.text = text;
+        widget.metadata = metadata;
+        widget = updateWidgetById(widgetId, widget);
+
+        if(widget)
+            res.redirect("/assignment/index.html#/user/"+userId+"/website/"+websiteId+"/page/"+pageId+"/widget/"+widgetId);
+        else res.redirect("back");
+    }
+
+    function getWidgetById(widgetId) {
+        var widget = undefined;
+        for(var w in widgets) {
+            if(widgets[w]._id === widgetId) {
+                widget = widgets[w];
+                break;
+            }
+        }
+        return widget;
+    }
+
+    function updateWidgetById(widgetId, widget) {
+        for(var w in widgets) {
+            if(widgets[w]._id === widgetId) {
+                widget._id = widgets[w]._id;
+                widget.pageId = widgets[w].pageId;
+                widgets[w] = widget;
+                return widget;
+            }
+        }
+        return null;
     }
 };
